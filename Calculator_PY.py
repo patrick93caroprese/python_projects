6121ad77e374f71ab6404e68ed3e4a8840d5f9d0
Patrick Caroprese <caropresepatrick@gmail.com>
2024-11-12 18:15:13 +0100

    Update Calculator.py

--- Calculator_PY.py
+++ Calculator_PY.py
@@ -19,7 +19,7 @@ def growth_rate (x,y):
 
 
 #Start building up the logic giving out the possible choices
-print("Choose your calculation:")
+print("CHOOSE YOUR CALCULATION")
 print("Addition")
 print("Subtraction")
 print("Multiplication")
@@ -30,44 +30,44 @@ print("Growth Rate")
 #Using a while loop for automatizing everything
 while True:
 
-    choice = input("Enter your choice: ")
+    choice = input("ENTER YOUR CHOISE: ")
 
     if choice.upper() in ("A","ADD","ADDITION",
                           "S","SUB","SUBTRACT","SUBTRACTION",
                           "M","MULT","MULTIPLY","MULTIPLICATION",
                           "D","DIV","DIVIDE", "DIVISION",
                           "P", "POWER",
-                          "G", "GROWTH", "GROWTH RATE"):
+                          "G", "GROWTH", "GROWTH RATE", "GROWTH_RATE"):
 
         num_1 = float(input("Enter first number: "))
 
         num_2 = float(input("Enter second number: "))
 
         if choice.upper() in ("A","ADD","ADDITION"):
-            print(num_1 , "+", num_2, "=", add(num_1,num_2))
+            print(num_1, "+", num_2, "=", add(num_1,num_2))
 
         elif choice.upper() in ("S","SUB","SUBTRACT","SUBTRACTION"):
-            print(num_1 , "-", num_2, "=", subtract(num_1,num_2))
+            print(num_1, "-", num_2, "=", subtract(num_1,num_2))
 
         elif choice.upper() in ("M","MULT","MULTIPLY","MULTIPLICATION"):
-            print(num_1 , "*", num_2, "=", multiply(num_1,num_2))
+            print(num_1, "*", num_2, "=", multiply(num_1,num_2))
 
         elif choice.upper() in ("D","DIV","DIVIDE", "DIVISION"):
-            print(num_1 , "/", num_2, "=", divide(num_1,num_2))
+            print(num_1, "/", num_2, "=", divide(num_1,num_2))
 
         elif choice.upper() in ("P", "POWER"):
             print(num_1, "to the power of", num_2, "=", power(num_1,num_2))
 
-        elif choice.upper() in ("G", "GROWTH", "GROWTH RATE"):
-            print("The growth rate from", num_1 , "to", num_2, "=", round(growth_rate(num_1,num_2),2), "%")
+        elif choice.upper() in ("G", "GROWTH", "GROWTH RATE", "GROWTH_RATE"):
+            print("The growth rate from", num_1, "to", num_2, "=", round(growth_rate(num_1,num_2),2), "%")
 
-    #Logic in case of a typing error
+    #Logic in case of a typo
     else:
         print("PLEASE TYPE THE CORRECT CHOICE")
 
     #Logic in case of a next calculation
-    next_calculation = input("Wanna do another calculation?: ")
+    next_calculation = input("Wanna do another calculation?: (yes / no) ")
 
-    #Logic for breaking the loop, ending the conversation
+    #Logic for breaking the loop
     if next_calculation.upper() in ("N", "NO", "NOPE", "NO WAY", "HELL NO", "THAT'S ENOUGH"):
         break
